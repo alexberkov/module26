85c8eaaf60850ec2e341cefa96260d6f018d5f9f
alexberkov <82799873+alexberkov@users.noreply.github.com>
2021-06-11 15:38:51 +0300

    Add files via upload

--- Task1/Player.h
+++ Task1/Player.h
@@ -7,6 +7,7 @@ using namespace std;
 class Player {
     vector<Track> playlist;
     int current = 0;
+    bool status = false;
 public:
     void addTrack();
     void play();
@@ -26,37 +27,52 @@ void Player::addTrack() {
 }
 
 void Player::play() {
-  if (current != 0) playlist[current - 1].stop();
+  if (current != 0) {
+    playlist[current - 1].stop();
+    status = false;
+  }
   int i;
   cout << "Enter track number: ";
   cin >> i;
   if (i > 0 && i <= playlist.size()) {
     playlist[i - 1].play();
+    status = true;
     current = i;
   } else cout << "No such track!" << endl;
 }
 
 void Player::pause() {
-  playlist[current - 1].pause();
+  if (status && current != 0) {
+    playlist[current - 1].pause();
+    status = false;
+  } else return;
 }
 
 void Player::resume() {
-  playlist[current - 1].resume();
+  if (!status && current != 0) {
+    playlist[current - 1].resume();
+    status = true;
+  } else return;
 }
 
 void Player::next() {
-  if (current != 0) playlist[current - 1].stop();
+  if (current != 0) {
+    playlist[current - 1].stop();
+    status = false;
+  }
   int prev = current;
   while (current == prev) {
     srand(time(nullptr));
     current = 1 + rand() % playlist.size();
   }
   playlist[current - 1].play();
+  status = true;
 }
 
 void Player::stop() {
-  if (current != 0) {
+  if (current != 0 && status) {
     playlist[current - 1].stop();
+    status = false;
     current = 0;
   } else return;
 }
--- Task1/Track.h
+++ Task1/Track.h
@@ -9,12 +9,11 @@ class Track {
     string name;
     int duration;
     tm birth{};
-    bool status;
 public:
     Track();
     void play();
-    void pause();
-    void resume();
+    static void pause();
+    static void resume();
     void stop();
     void showDuration() const;
     friend class Player;
@@ -26,7 +25,6 @@ Track::Track() {
   name = "NULL";
   duration = 0;
   birth = *localtime(&t);
-  status = false;
 }
 
 void Track::showDuration() const {
@@ -36,29 +34,19 @@ void Track::showDuration() const {
 }
 
 void Track::play() {
-  if (!status) {
     cout << "Now playing: " << name << ", ";
     this->showDuration();
     cout << ", created: " << put_time(&birth, "%Y/%m/%d %H:%M:%S") << endl;
-    status = true;
-  } else return;
 }
 
 void Track::pause() {
-  if (status)  {
     cout << "Paused || " << endl;
-    status = false;
-  } else return;
 }
 
 void Track::resume() {
-  if (!status) {
     cout << "Playing |>" << endl;
-    status = true;
-  } else return;
 }
 
 void Track::stop() {
-  if (status)  status = false;
   cout << "Stopped: " << name << endl;
 }
